125a4ad228183295e491c098bacd16fa32fac6ba
mozzilla <mozzilla105>
2022-11-03 17:55:32 +0300

    initial commit

---
+++ exercise013/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу,которая выводит третью цифру заданного числа
+//или сообщает,что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if (num1 / 100 >= 1)
+{
+    while (num1 > 1000)
+    {
+        num1 = num1 / 10;
+    }
+    int num3 = num1 % 10;
+    Console.WriteLine(num3);
+}
+else
+{
+    Console.WriteLine("Ваше число меньше чем трёхзначное.");
+}
+
+
