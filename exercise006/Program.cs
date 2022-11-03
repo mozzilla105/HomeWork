b0f9192677e8b6144d4aacabf67b3522e998c1db
mozzilla <mozzilla105>
2022-11-03 17:55:09 +0300

    initial commit

---
+++ exercise006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6 Напишите программу,которая на вход принимает число и выдаёт,является ли число чётным.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number%2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число является нечётным");  
+}
