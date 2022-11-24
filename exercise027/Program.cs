66fb3480de7c3a1ef7dd7b49e84d222fd7d9474f
mozzilla <mozzilla105>
2022-11-24 15:55:01 +0300

    initial commit

---
+++ exercise027/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание 27. Напишите функцию и запишите её,
+// которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNum(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+        result = result + num%10;
+        num = num/10;
+    }
+    return result;
+}
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNum(N));
