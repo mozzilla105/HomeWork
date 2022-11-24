70d4a77aecfcdd1390b9a50dcd694dc463f7f1df
mozzilla <mozzilla105>
2022-11-24 15:55:11 +0300

    initial commit

---
+++ exercise029/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задание 29. Напишите программу,которая задаёт
+// массив из 8 элементов и выдаёт их на экран.
+int FillArray(int[] mass)
+{
+    int length = mass.Length;
+    for (int i = 0; i < length; i++)
+    {
+        mass[i] = int.Parse(Console.ReadLine()!);
+    }
+    return length;
+}
+int PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int ind = 0; ind < count-1; ind++)
+    {
+        Console.Write($"{col[ind]},");
+    }
+    Console.Write(col[7]);
+    return count;
+}
+Console.WriteLine("Введите элементы массива");
+int[] array = new int [8];
+FillArray(array);
+Console.Write("Массив: [");
+PrintArray(array);
+Console.Write("]");
