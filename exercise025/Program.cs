050af15d8db99434baf8d3ed5da0ca5280dd3b72
mozzilla <mozzilla105>
2022-11-24 15:54:21 +0300

    initial commit

---
+++ exercise025/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задание 25. Напишите цикл,который принимает на вход числа (А и В) и 
+// и возводит А в натуральную степень В.
+int Degree(int a, int b)
+{
+   
+    int degree = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        degree = degree * a;
+    }
+    return degree;
+}
+Console.WriteLine("Введите первое число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Degree(N,M));
