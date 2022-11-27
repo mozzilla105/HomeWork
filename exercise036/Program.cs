1d1c90a11167469347381051dad80cf36f03ff12
mozzilla <mozzilla105>
2022-11-28 00:43:03 +0300

    initial commit

---
+++ exercise036/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задание 36. одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов,стоящих на нечётных позициях.
+
+int[] Returns_Array(int size, int LeftValue, int RightValue)
+{
+    int[] vector = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        vector[i] = new Random().Next(LeftValue, RightValue + 1);
+    }
+    return vector;
+}
+
+int Sum_of_Elements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 !=0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение левого конца диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение правого конца диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+int[] WorkingArray = Returns_Array(number, a, b);
+Console.WriteLine($"Полученный массив: [{String.Join(",", WorkingArray)}]");
+Console.WriteLine($"Сумма элементов массива,стоящих на нечётных позициях равна: {Sum_of_Elements(WorkingArray)}");
