dc8b22c5d3629ead8e20f5a2bf0ff593f9810cf8
mozzilla <mozzilla105>
2022-12-02 10:59:02 +0300

    initial commit

---
+++ exercise041/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] myArray = InputArray();
+Console.WriteLine($"{ArrayToString(myArray)} -> {GetPisitiveCount(myArray)}");
+
+int GetPisitiveCount(int[] array) 
+{
+    int count = 0;
+    foreach (int item in array) { if (item > 0) count++; }
+    return count;
+}
+
+int[] InputArray() 
+{
+    Console.Write("Введите массив из N чисел, разделяя запятыми: ");
+    string[] numbers = Console.ReadLine()!.Trim(' ',',').Split(",");
+    int[] array = new int[numbers.Length];
+    int i = 0;
+    foreach (var item in numbers)
+    {
+        array[i] = Convert.ToInt32(item); i++;
+    }
+    return array;
+}
+String ArrayToString(int[] array) { return "[" + String.Join(", ", array) + "]"; }
