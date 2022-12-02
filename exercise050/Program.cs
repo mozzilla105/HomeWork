23e312b6370abe44a90f32d4cb1e67ce09e91824
mozzilla <mozzilla105>
2022-12-02 11:00:04 +0300

    initial commit

---
+++ exercise050/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает число и ищет его в двумерном массиве. 
+//Программа должна возвращать значение позиции (номер строки и столбца) этого элемента или же указание, что такого элемента нет.
+
+
+
+bool SearchNumberInArray(int[,] array, int findNumber)
+{
+
+    bool isFound = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == findNumber)
+            {
+                Console.Write((isFound ? "" : "Найдено:\n"));
+                Console.WriteLine($"[{i},{j}]");
+                isFound = true;
+            }
+        }
+    }
+    return isFound;
+}
+
+
+
+
+int[,] myTestArray = { { 12, 4, 17, 32 },
+                       { 5, 9, 24, 1 },
+                       { 8, 5, 2, 14}};
+
+Console.WriteLine("Введите число для поиска: ");
+int findNum = int.Parse(Console.ReadLine());
+bool resultSearch = SearchNumberInArray(myTestArray, findNum);
+if (!resultSearch)
+{
+    Console.WriteLine($"Элемент со значением {findNum} не найдены");
+}
