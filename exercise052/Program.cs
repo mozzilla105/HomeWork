ad86b8539023912e34946488cb5af4ec83af0cd4
mozzilla <mozzilla105>
2022-12-02 11:00:44 +0300

    initial commit

---
+++ exercise052/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int dimA, int dimB, int minVal, int maxVal)
+{
+    Random rnd = new Random();
+    int[,] newArray = new int[dimA, dimB];
+    for (int i = 0; i < dimA; i++)
+    {
+        for (int j = 0; j < dimB; j++)
+        {
+            newArray[i, j] = rnd.Next(minVal, maxVal);
+        }
+    }
+    return newArray;
+}
+
+
+void PrintIntArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMeanColumnArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int summ = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            summ += array[i, j];
+        }
+        summ /= array.GetLength(0);
+        Console.Write($"{summ,5} ");
+    }
+}
+
+int[,] myArray = GetArray(4, 4, -10, 10);
+PrintIntArray(myArray);
+Console.WriteLine("   --------------------");
+ArithmeticMeanColumnArray(myArray);
